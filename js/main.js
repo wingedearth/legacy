4120647fce64d9bd91a01e0e56b33e8a94ce4dca
Andrew Anissi <andrew@wingedearth.com>
2016-03-10 12:46:02 -0800

    create shells and TODO tasks for Legacy Challenge specs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,60 @@
+var MyButton = React.createClass({
+  getInitialState: function() {
+    return {coloring: white};
+  },
+
+  clicked: function() {
+    this.props.clickHandler
+  },
+
+// Maybe add color changing buttons later. - Icebox
+  // red: function() {
+  //   return {coloring: red};
+  // },
+
+  // green: function() {
+  //   return {coloring: green};
+  // },
+
+  // white: function() {
+  //   return {coloring: white};
+  // },
+
+  render: function() {
+    return(
+      <div>
+      <button
+        onClick={this.clicked}
+        className="btn btn-floating waves-effect waves-light">
+        <i className="material-icons">create</i>
+      </button>
+    </div>
+      )
+  }
+});
+
 var MyComponent = React.createClass({
 
+  getInitialState: function() {
+    return {showText: true};
+  },
+
+  handleButtonClick: function() {
+    // TODO
+  },
+
+  render: function() {
+    return(
+      <div className='container'>
+        <span>{this.props.text}</span>
+      </div>
+    );
+  }
 });
 
-var MyButton = React.createClass({
 
-});
+
+React.render(<MyComponent text="I'm only borrowing your Humvee."></MyComponent>,
+  document.getElementById('content')
+);
 
